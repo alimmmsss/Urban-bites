3b8d89d58e36385bae7fcec2cc48aef835a6f921
unknown <alim111973@gmail.com>
2025-12-04 22:16:53 +0600

    Fix middleware auth error

--- middleware.ts
+++ middleware.ts
@@ -16,7 +16,7 @@ const isPublicRoute = createRouteMatcher([
 export default clerkMiddleware(async (auth, request) => {
   // Protect all routes except public ones
   if (!isPublicRoute(request)) {
-    await auth.protect();
+    auth().protect();
   }
 });
 
